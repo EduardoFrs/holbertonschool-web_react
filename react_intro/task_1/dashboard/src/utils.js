e907cc831fbf7bfdc711827d215be44e39c0dd5a
EduardoFrs <fernandeseduardo00@gmail.com>
2026-08-24 09:29:39 +0200

    feat: 2. Embedding expressions, functions

--- react_intro/task_1/dashboard/src/utils.js
+++ react_intro/task_1/dashboard/src/utils.js
@@ -1,7 +1,10 @@
 export function getCurrentYear() {
-  return new Date().getFullYear()
+  return new Date().getFullYear();
 }
 
 export function getFooterCopy(isIndex) {
-  return isIndex ? 'Holberton School' : 'Holberton School main dashboard'
+  if (isIndex) {
+    return 'Holberton School';
+  }
+  return 'Holberton School main dashboard';
 }
